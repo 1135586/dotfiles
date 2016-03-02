0823f9e73867fc5cc69e28e24c970ce2c4853c6e
dmitry <1135586@gmail.com>
2016-03-02 11:15:20 +0600

    added 2 scripts to run idea(from sudo su) and android studio

---
+++ idea.sh
@@ -0,0 +1,4 @@
+service docker start
+cd /usr/local/idea/bin/
+export JAVA_HOME=/usr/lib/jdk/
+./idea.sh
---
+++ studio.sh
@@ -0,0 +1,3 @@
+cd /usr/local/android-studio/bin/
+#export JAVA_HOME=/usr/lib/jdk/
+./studio.sh
